4d338985a78ebdceafd39c241d9e8450f955c8bf
narasimhulu <narasimhulu3k@gmail.com>
2024-10-22 21:01:47 +0530

    4th commit

--- src/test/java/SeleniumFramework/Tests/testdemo1q.java
+++ src/test/java/SeleniumFramework/Tests/testdemo1q.java
@@ -6,6 +6,10 @@ public class testdemo1q {
 		System.out.println("testdemo");
 		System.out.println("testdemo_YY");
 		System.out.println("testdemo_XX");
+		System.out.println("testdemo4th_YY");
+		System.out.println("testdemo4TH_XX");
+		System.out.println("testdemo4th_YY");
+		System.out.println("testdemo4TH_XX");
 	}
 
 }
