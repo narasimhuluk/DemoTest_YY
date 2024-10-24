0be972e0ff4664646ab2d9badcb13cf5ac72bb17
narasimha <narasimhulu3k@gmail.com>
2024-10-24 22:56:04 +0530

    jenkinsUpdate

--- src/test/java/SeleniumFramework/Tests/testdemo1q.java
+++ src/test/java/SeleniumFramework/Tests/testdemo1q.java
@@ -10,6 +10,10 @@ public class testdemo1q {
 		System.out.println("testdemo4TH_XX");
 		System.out.println("testdemo4th_YY");
 		System.out.println("testdemo4TH_XX");
+		System.out.println("testdemo4th_YY");
+		System.out.println("testdemo4TH_XX");
+		System.out.println("testdemo4th_YY");
+		System.out.println("testdemo4TH_XX");
 	}
 
 }
